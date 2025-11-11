52d58e961d8392d16ba199d6f885aaf837b310b9
gilli-jonathan <gilli.jonathan97@gmail.com>
2025-11-11 11:28:55 +0100

    impost movie array

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,16 @@
 
 function App() {
 
+  const movies = [
+    { title: 'Inception', genre: 'Fantascienza' },
+    { title: 'Il Padrino', genre: 'Thriller' },
+    { title: 'Titanic', genre: 'Romantico' },
+    { title: 'Batman', genre: 'Azione' },
+    { title: 'Interstellar', genre: 'Fantascienza' },
+    { title: 'Pulp Fiction', genre: 'Thriller' },
+  ]
+
+
 
   return (
     <>
